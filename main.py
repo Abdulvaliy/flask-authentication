72c2642784b62ca01f0567f1e14c9ba2405fdb59
Abduvali Zokhidov <80103407+Abdulvaliy@users.noreply.github.com>
2021-10-12 00:03:05 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,105 @@
+from flask import Flask, render_template, request, url_for, redirect, flash, send_from_directory
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'any-secret-key-you-choose'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+##CREATE TABLE IN DB
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+    name = db.Column(db.String(1000))    # Line below only required once, when creating DB.
+# db.create_all()
+
+
+@app.route('/')
+def home():
+    return render_template("index.html", logged_in=current_user.is_authenticated)
+
+
+@app.route('/register', methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        register_user = User(
+            name=request.form["name"],
+            email=request.form["email"],
+            # password=request.form["password"]    # plain-text password
+        )
+        register_user.password = generate_password_hash(request.form["password"], method='pbkdf2:sha256', salt_length=8)
+        user = User.query.filter_by(email=request.form["email"]).first()
+        if user:
+            flash("You've already signed up with that email! Log in instead.")
+            return redirect(url_for('login'))
+        else:
+            db.session.add(register_user)
+            db.session.commit()
+            login_user(register_user)
+            return render_template("secrets.html", name=register_user.name.title(), logged_in=current_user.is_authenticated)
+    return render_template("register.html", logged_in=current_user.is_authenticated)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == "POST":
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        # Find user by email entered.
+        user = User.query.filter_by(email=email).first()
+
+        # Email doesn't exist
+        if not user:
+            flash('That email does not exist. Please, try again.')
+            redirect(url_for('login'))
+
+        # Password incorrect
+        elif not check_password_hash(user.password, password):
+            flash('Incorrect password. Please try again')
+            return redirect(url_for('login'))
+
+        # Email exist and password is correct
+        else:
+            login_user(user)
+            print(user.name)
+            return redirect(url_for('secrets', logged_in=True))
+
+    return render_template("login.html", logged_in=current_user.is_authenticated)
+
+
+@app.route('/secrets')
+@login_required
+def secrets():
+    print(current_user.name)
+    return render_template("secrets.html", name=current_user.name.title(), logged_in=True)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+
+@app.route('/download', methods=["GET"])
+@login_required
+def download():
+    return send_from_directory('static', "files/cheat_sheet.pdf")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
